8a7f9d59f7ba34908fb1b98a04f5727825a9125b
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-03-02 19:52:33 -0500

    commit tree c9443c002f736ad79483e482459fa81988a59ace

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,45 @@
+require 'pry'
 class Transfer
-  # your code here
+attr_reader :sender, :receiver, :amount
+attr_accessor :status
+
+
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+    @sender.valid? && @receiver.valid? ? true : false
+  end
+
+  def execute_transaction
+    if @status == "pending"
+    #  binding.pry
+      if (@sender.balance - @amount) < 0 || self.valid? == false
+        @status=("rejected")
+        "Transaction rejected. Please check your account balance."
+      else self.valid?
+        money_to = 0 + @amount
+        money_from = 0 - @amount
+        @sender.deposit(money_from)
+        @receiver.deposit(money_to)
+        @status=("complete")
+      end
+    end
+  end
+
+  def reverse_transfer
+    if @status == "complete"
+      money_to = 0 + @amount
+      money_from = 0 - amount
+      @sender.deposit(money_to)
+      @receiver.deposit(money_from)
+      @status=("reversed")
+    end
+  end
+
 end
